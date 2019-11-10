f0de1f22e96489c804af23481be56553813eae32
Lucio Paiva <luciopaiva@gmail.com>
2019-11-10 19:21:46 +0000

    Support titles with trailing hashes, add UI to specify max ToC level

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ class MarkdownToc {
     constructor () {
         this.inputArea = document.getElementById("input-area");
         this.outputArea = document.getElementById("output-area");
+        this.levelUpButton = document.getElementById("level-up");
+        this.levelUpButton.addEventListener("click", this.changeLevel.bind(this, +1));
+        this.levelDownButton = document.getElementById("level-down");
+        this.levelDownButton.addEventListener("click", this.changeLevel.bind(this, -1));
+        this.levelsToShowElement = document.getElementById("levels-to-show");
+        this.levelsToShow = 2;
 
         this.inputArea.addEventListener("input", this.process.bind(this));
+        this.process();
+    }
+
+    changeLevel(delta) {
+        this.levelsToShow += delta;
+        if (this.levelsToShow < 1) {
+            this.levelsToShow = 1;
+        } else if (this.levelsToShow > 10) {
+            this.levelsToShow = 10;
+        } else if (typeof this.levelsToShow !== "number") {
+            this.levelsToShow = 2;
+        }
+        this.levelsToShowElement.setAttribute("value", this.levelsToShow);
         this.process();
     }
 
@@ -13,6 +32,7 @@ class MarkdownToc {
         const input = this.inputArea.value;
         const menus = ["# Table of contents", ""];
         let isCodeBlock = false;
+        let topLevel = NaN;
 
         for (let line of input.split("\n")) {
 
@@ -25,13 +45,21 @@ class MarkdownToc {
             }
 
             if (line.startsWith("#")) {
-                const match = line.match(/(#+)\s*(.*)/);
-                const level = match[1].length - 1;
-                const title = match[2];
+                const match = line.match(/(#+)\s*(.*?)#*\s*$/);
+                const level = match[1].length;
+                if (isNaN(topLevel)) {
+                    topLevel = level;
+                }
+
+                if (level - topLevel >= this.levelsToShow) {
+                    continue;
+                }
+
+                const title = match[2].trim();
                 const link = title.toLocaleLowerCase()
                     .replace(/\s/g, "-")
                     .replace(/[^A-Za-z0-9-]/g, "");
-                const menu = `${"  ".repeat(level)}- [${title}](#${link})`;
+                const menu = `${"  ".repeat(level - topLevel)}- [${title}](#${link})`;
                 menus.push(menu);
             }
         }
