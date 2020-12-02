11d63f9058f44ec759a4669fab60a653188037e1
Lucio Paiva <luciopaiva@gmail.com>
2020-12-02 21:24:14 +0000

    Preserve underscores in titles

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ class MarkdownToc {
 
                 const link = title.toLocaleLowerCase()
                     .replace(/\s/g, "-")
-                    .replace(/[^A-Za-z0-9-]/g, "");
+                    .replace(/[^A-Za-z0-9-_]/g, "");
                 const menu = `${"  ".repeat(level - topLevel)}- [${title}](#${link})`;
                 menus.push(menu);
 
